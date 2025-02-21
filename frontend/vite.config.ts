cec75bbe99335dcd5e87019309e6b6ea99f7648d
Cohen <brendanintech@gmail.com>
2025-02-21 15:49:24 -0500

    initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    base: "/",
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            injectHead: process.env.VITE_IS_DEPLOYED
+              ? '<script defer="" data-domain="screenshottocode.com" src="https://plausible.io/js/script.js"></script>'
+              : "",
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom', 'react-router-dom'],
+            ui: ['@radix-ui/react-accordion', '@radix-ui/react-dialog', '@radix-ui/react-popover'],
+          },
+        },
+      },
+    },
+    server: {
+      host: true,
+      port: 3000,
+    },
+  });
+};
